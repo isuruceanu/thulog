69df7295a0706112132e11a3411f0e4fcb339983
isuruceanu <surucion@gmail.com>
2014-06-25 20:54:28 +0300

    start working on c# exemple

---
+++ src/console/c#/thulog.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text;
+using LibUsbDotNet;
+using LibUsbDotNet.Main;
+
+//source : http://libusbdotnet.sourceforge.net/V2/Index.html
+
+namespace ThulogExample
+{
+	public class Thulog
+	{
+		private static UsbDevice _usbDevice;
+
+		public static UsbDeviceFinder myUsbFinder = new UsbDeviceFinder(1234,1);
+
+		public static void Main
+	}
+
+
+}
